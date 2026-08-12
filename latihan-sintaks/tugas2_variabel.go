bae5f5b2ffc040f5819d51d1bc2a14c32871d8c8
naisyazh <naisyazh@gmail.com>
2026-08-12 23:31:50 +0700

    Menambahkan tugas 2 bagian variabel - 5 variabel dengan tipe berbeda

---
+++ latihan-sintaks/tugas2_variabel.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var nama string = "Naisya"
+	umur := 20
+	
+ipk := 3.75                          
+aktif := true                        
+hobi := []string{"scroll", "tiktok"} 
+	
+	fmt.Println("Nama:", nama)
+	fmt.Println("Umur:", umur)
+	fmt.Printf("IPK: %.2f\n", ipk)
+	fmt.Println("Aktif:", aktif)
+	fmt.Println("Hobi:", hobi)
+	
+
+
